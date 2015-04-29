49d400d3de293f53ed0432eac671c7c1e308b085
Chan Y. Park <phygeeks@gmail.com>
2015-04-29 18:59:48 -0400

    produce both local and web plots

--- cproj.py
+++ cproj.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
 
-import sys
+import sys, getopt
 import subprocess
 
 import matplotlib.pyplot as pyplot
 import mpld3
+import mpldatacursor
 
 import pdb
 
 root_system = sys.argv[1]
 n_of_v_0 = sys.argv[2]
+
+shortopts = "i:`"
+longopts = [
+    "use-mpld3",
+]
+
+optlist, args = getopt.getopt(sys.argv[3:], shortopts, longopts)
+
+# Default values of options
+weight_index = None
+use_mpld3 = False
+for opt, arg in optlist:
+    if(opt == '-i'):
+        weight_index = int(arg)
+    elif(opt == "--use-mpld3"):
+        use_mpld3 = True
 
 #root_system = 'A3
 #n_of_v_0 = 2
@@ -35,7 +52,7 @@ len_orbit = len(weyl_orbit)
 title = root_system + "_" + str(n_of_v_0)
 
 # Save the soliton table as a text file.
-with open(title + ".txt", 'w') as f:
+with open("./results/" + title + ".txt", 'w') as f:
     # List of weights 
     for i, v_i in enumerate(weyl_orbit):
         f.write("v_{} = {}\n".format(i, v_i))
@@ -61,7 +78,7 @@ with open(title + ".txt", 'w') as f:
         f.write('\n')
 
 # Plot a figure of the projection of the soliton polytope.
-figure = pyplot.figure(title)
+figure = pyplot.figure(title, facecolor='w')
 pyplot.title(title)
 pyplot.axis('off')
 #pyplot.tick_params(
@@ -77,28 +94,74 @@ pyplot.axis('off')
 pyplot.axes().set_aspect('equal')
 
 # Plot critical points.
+W_point_labels = ["v_{} = {}".format(i, v_i) 
+                  for i, v_i in enumerate(weyl_orbit)]
 W_points = pyplot.plot(
     [W_i.real for W_i in W_critical], 
     [W_i.imag for W_i in W_critical],
     'o',
-    color='k'
+    markersize=10,
+    color='k',
+    label=W_point_labels,
 )
-W_point_labels = [str(i) for i in range(len_orbit)]
-
-W_point_tooltip = mpld3.plugins.PointLabelTooltip(
-    W_points[0], W_point_labels,
-)
-mpld3.plugins.connect(figure, W_point_tooltip)
-
-# Plot solitons connecting critical points.
+W_points = []
 for i in range(len_orbit):
     W_i = W_critical[i]
+    mplobjs = pyplot.plot(
+        W_i.real, 
+        W_i.imag,
+        'o',
+        markersize=10,
+        color='k',
+        label=W_point_labels[i],
+    )
+    W_points.append(mplobjs[0])
+
+
+# Plot solitons connecting critical points.
+soliton_labels = []
+soliton_lines = []
+
+if weight_index is None:
+    i_list = range(len_orbit)
+else:
+    i_list = [weight_index]
+
+for i in i_list:
+    W_i = W_critical[i]
     for j in range(len_orbit):
-        soliton = simple_soliton_table[i][j]
-        if soliton is None:
+        soliton_data = simple_soliton_table[i][j]
+        if soliton_data is None:
             continue
+        else:
+            soliton, sign = soliton_data
         W_j = W_critical[j]
-        pyplot.plot([W_i.real, W_j.real], [W_i.imag, W_j.imag], '-')
- 
-#pyplot.show()
-mpld3.show()
+        label = "alpha_{} = {}".format(soliton, roots[soliton])
+        mplobjs = pyplot.plot(
+            [W_i.real, W_j.real], [W_i.imag, W_j.imag], '-',
+            label=label,
+        )
+        soliton_lines.append(mplobjs[0])
+        soliton_labels.append(label)
+
+if use_mpld3 is True:
+    for i, point in enumerate(W_points):
+        W_point_tooltip = mpld3.plugins.PointLabelTooltip(
+            point, [W_point_labels[i]],
+        )
+        mpld3.plugins.connect(figure, W_point_tooltip)
+
+    for i, soliton in enumerate(soliton_lines):
+        soliton_tooltip = mpld3.plugins.LineLabelTooltip(
+            soliton, [soliton_labels[i]],
+        )
+        mpld3.plugins.connect(figure, soliton_tooltip)
+
+    mpld3.show()
+else:
+    mpldatacursor.datacursor(
+        formatter="{label}".format,
+        display="multiple",
+        draggable=True,
+    )
+    pyplot.show()
