30ccad4b0390a670a0220ba0166a50b40760e9e8
Chan Y. Park <phygeeks@gmail.com>
2015-06-05 14:44:43 -0400

    simplified web frontend

--- cproj.py
+++ cproj.py
@@ -1,8 +1,211 @@
 #!/usr/bin/env python
 
-import sys, getopt
+import os, sys, getopt
+import subprocess
+import random
+from math import ceil, log10
+from io import BytesIO
 import pdb
 from coxeter_projection import CoxeterProjection
+
+DATA_DIR = './data/'
+
+
+def set_sage_data(root_system=None, n_of_v_0_str=None,):
+    data_file_path = DATA_DIR + root_system + '_' + n_of_v_0_str
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
+    if not os.path.isfile(data_file_path):
+        sage_data_str = subprocess.check_output(
+            ["sage", "cproj.sage", root_system, n_of_v_0_str] 
+        )
+        with open(data_file_path, 'w') as f:
+            f.write(sage_data_str)
+
+def get_sage_data_str(root_system=None, n_of_v_0_str=None,):
+    data_file_path = DATA_DIR + root_system + '_' + n_of_v_0_str
+    with open(data_file_path, 'r') as f:
+        sage_data_str = f.read()
+    return sage_data_str
+
+
+def plot_coxeter_projection(
+    sage_data_str, root_system=None, n_of_v_0=None, weight_index=None,
+    is_interactive=True,
+):
+    #XXX: Is there a way to put these imports in the beginning of a 
+    #     module?
+    import matplotlib
+    if is_interactive is True:
+        matplotlib.use('TkAgg')
+    else:
+        matplotlib.use('Agg')
+    import mpldatacursor
+    import matplotlib.pyplot as pyplot
+
+    matplotlib.rcParams["savefig.directory"] = "./"
+
+    # Unpack data from the sage script.
+    data = eval(sage_data_str)
+    weyl_orbit = data["weyl_orbit"]
+    W_critical = data["W_critical"]
+    roots = data["roots"]
+    simple_soliton_table = data["simple_soliton_table"]
+    v_c = data["coxeter_vector"]
+
+    title = root_system + "_" + str(n_of_v_0)
+
+    # Plot a figure of the projection of the soliton polytope.
+    figure = pyplot.figure(
+        title, facecolor='w', figsize=(8, 8), 
+        #dpi=100,
+    )
+    pyplot.axis('off')
+
+    pyplot.axes().set_aspect('equal')
+
+    mpldatacursor_artists = []
+
+    # Plot critical points.
+    W_marker_size = 25
+    W_font_size = 18.0/ceil(log10(len(weyl_orbit)))
+    W_point_labels = ["$v_{{{}}} = {}$".format(i, v_i) 
+                      for i, v_i in enumerate(weyl_orbit)]
+
+    grouped_W_c = group_degenerate_W_c(W_critical)
+    for W, indices in grouped_W_c:
+        mplobjs = pyplot.plot(
+            W.real, W.imag,
+            'o',
+            markersize=W_marker_size,
+            markeredgewidth=1.5,
+            markerfacecolor='w',
+            color='k',
+            label=', '.join(W_point_labels[i] for i in indices),
+        )
+        pyplot.text(
+            W.real, W.imag,
+            ','.join(str(i+1) for i in indices),
+            fontsize=W_font_size/len(indices),
+            verticalalignment='center',
+            horizontalalignment='center',
+        )
+
+        if is_interactive:
+            mpldatacursor_artists.append(mplobjs[0])
+
+    # Plot solitons connecting critical points.
+    soliton_colormap = matplotlib.cm.ScalarMappable(
+        norm=matplotlib.colors.Normalize(vmin=0, vmax=len(roots)),
+        cmap=matplotlib.cm.get_cmap('jet'), 
+    )
+    soliton_colors = [soliton_colormap.to_rgba(i)
+                      for i in range(len(roots))]
+    random.shuffle(soliton_colors)
+    #colors = ['b', 'r', 'g', 'c', 'm', 'y']
+    #soliton_colors = [colors[i%6] for i in range(len(roots))]
+
+
+    if (weight_index is None 
+        or weight_index > len(weyl_orbit) 
+        or weight_index < 1):
+        i_list = range(len(weyl_orbit))
+    else:
+        i_list = [weight_index-1]
+
+    for i in i_list:
+        W_i = W_critical[i]
+        for j in range(len(weyl_orbit)):
+            #if j <= i:
+            #    continue
+            soliton_data = simple_soliton_table[i][j]
+            if soliton_data is None:
+                continue
+            else:
+                soliton, sign = soliton_data
+            W_j = W_critical[j]
+            if sign == -1:
+                sign_str = '-'
+            else:
+                sign_str = ''
+            root = tuple([sign*r_k for r_k in roots[soliton]])
+            label = "${}\\alpha_{{{}}} = {}$".format(
+                sign_str, soliton, root
+            )
+            # Draw an arrow from v_i to v_j
+#                mplobjs = pyplot.annotate(
+#                    '',
+#                    xy=(W_j.real, W_j.imag),
+#                    xytext=(W_i.real, W_i.imag),
+#                    arrowprops=dict(
+#                        edgecolor=None,
+#                        facecolor=soliton_colors[soliton], 
+#                        shrink=.1,
+#                    )
+#                )
+            offset = .075
+            x = W_i.real
+            y = W_i.imag
+            dx = W_j.real - x
+            dy = W_j.imag - y
+            mplobjs = pyplot.arrow(
+                x + (offset * dx), 
+                y + (offset * dy), 
+                (1 - 2*offset)*dx, 
+                (1 - 2*offset)*dy, 
+                length_includes_head=True,
+                #shape='left',
+                width=.002,
+                head_width=.03,
+                head_length=.045,
+                label=label, color=soliton_colors[soliton],
+            )
+            # Draw a line from v_i to v_j
+            #mplobjs = pyplot.plot(
+            #    [W_i.real, W_j.real], [W_i.imag, W_j.imag], '-',
+            #    label=label, color=soliton_colors[soliton],
+            #)[0]
+            mpldatacursor_artists.append(mplobjs)
+
+    mpldatacursor.datacursor(
+        artists=mpldatacursor_artists,
+        formatter="{label}".format,
+        display="multiple",
+        draggable=True,
+    )
+
+    pyplot.margins(.05)
+
+    if is_interactive is True:
+        # Display the plot.
+        pyplot.show()
+    else:
+        # Return PNG to web frontend.
+        #img = StringIO()
+        img = BytesIO()
+        pyplot.savefig(img)
+        img.seek(0)
+        return img
+
+
+def group_degenerate_W_c(W_c):
+    # grouped_W_c = [[W_i, [i_1, i_2, ...]], [W_j, [j_1, j_2, ...]], ...]
+    grouped_W_c = []
+    epsilon = 1e-6
+    for i, W_i in enumerate(W_c):
+        is_new = True
+        for j, W_group in enumerate(grouped_W_c):
+            W = W_group[0]
+            if abs(W_i - W) < epsilon:
+                W_group[1].append(i)
+                is_new = False
+                break
+        if is_new:
+            grouped_W_c.append([W_i, [i]])
+
+    return grouped_W_c
+            
+
 
 if __name__ == '__main__':
     #root_system = input("Enter the root system (e.g. 'A3', 'D4'): ")
@@ -22,6 +225,6 @@ if __name__ == '__main__':
         if(opt == '-i'):
             weight_index = int(arg)
 
-    cp = CoxeterProjection(root_system, n_of_v_0)
-    cp.get_sage_data()
-    cp.plot(weight_index)
+    set_sage_data(root_system, n_of_v_0)
+    sage_data_str = get_sage_data_str(root_system, n_of_v_0)
+    plot_coxeter_projection(sage_data_str, root_system, n_of_v_0, weight_index)
--- web_ui.py
+++ web_ui.py
@@ -3,7 +3,6 @@ import multiprocessing
 import time
 import pdb
 import cproj
-from coxeter_projection import CoxeterProjection, web_process
 
 # configuration
 DEBUG = True
@@ -14,40 +13,122 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def show_welcome():
-    #return flask.render_template('index.html')
     return flask.redirect(flask.url_for('get_config'))
 
 @app.route('/config', methods=['GET', 'POST'])
 def get_config():
-    flask.session['root_system'] = None
-    flask.session['n_of_v_0'] = None
-    flask.session['weight_index'] = None
+    is_submitted = False
+    config_items = {}
     if flask.request.method == 'GET' and bool(flask.request.args):
-        set_config_items(flask.session, flask.request.args)
+        set_config_items(config_items, flask.request.args)
     elif flask.request.method == 'POST':
-        set_config_items(flask.session, flask.request.form)
-    if flask.session['root_system'] is not None:
-        return flask.redirect(flask.url_for('show_result'))
+        set_config_items(config_items, flask.request.form)
+
+    if bool(config_items):
+        if config_items['type'] == 'E':
+            rank = int(config_items['rank'])
+            if (rank != 6 and rank != 7 and rank != 8):
+                flask.flash('The rank of an E-type should be 6, 7, or 8.')
+                return flask.redirect(flask.url_for('get_config'))
+            elif int(config_items['n_of_v_0']) != 1:
+                flask.flash(
+                    'Only 1 is allowed for the index of the fundamental '
+                    'weight for {}.'.format(config_items['root_system'])
+                )
+                return flask.redirect(flask.url_for('get_config'))
+        cproj.set_sage_data(
+            root_system=config_items['root_system'],
+            n_of_v_0_str=config_items['n_of_v_0']
+        )
+        return flask.redirect(
+            flask.url_for(
+                'show_result',
+                root_system=config_items['root_system'],
+                n_of_v_0=config_items['n_of_v_0'],
+                weight_index=config_items['weight_index'],
+            )
+        )
+        
     return flask.render_template('config.html')
 
 
 @app.route('/result')
 def show_result():
+    root_system = flask.request.args.get('root_system')
+    n_of_v_0_str = flask.request.args.get('n_of_v_0')
+    n_of_v_0 = int(n_of_v_0_str)
+    weight_index_str = flask.request.args.get('weight_index')
+    try:
+        weight_index = int(weight_index_str)
+    except ValueError:
+        weight_index = None
+    
+    sage_data_str = cproj.get_sage_data_str(root_system, n_of_v_0_str)
+    data = eval(sage_data_str)
+    weyl_orbit = data["weyl_orbit"]
+    W_critical = data["W_critical"]
+    roots = data["roots"]
+    simple_soliton_table = data["simple_soliton_table"]
+    v_c = data["coxeter_vector"]
+
+    len_orbit = len(weyl_orbit)
+    # Prepare v_c data
+    v_c_str = '(' + ', '.join(
+        '{:.3f}{:+.3f}i'.format(v_c_i.real, v_c_i.imag) for v_c_i in v_c
+    ) + ')'
+    # Prepare W_c data.
+    W_c_str = ['{:.3f}{:+.3f}i'.format(W_c.real, W_c.imag)
+        for W_c in W_critical
+    ]
+    # Prepare soliton data.
+    solitons_str = [
+        [None for i in range(len_orbit)] for i in range(len_orbit)
+    ]
+    for i, row in enumerate(simple_soliton_table):
+        for j, entry in enumerate(row):
+            if entry == None:
+                solitons_str[i][j] = ''
+                continue
+
+            k, sign = entry
+            if sign == -1:
+                sign_str = '-'
+            else:
+                sign_str = ''
+            solitons_str[i][j] = '{}\\alpha_{{ {} }}'.format(sign_str, k+1)
+
     return flask.render_template(
         'result.html', 
+        root_system=root_system,
+        n_of_v_0=n_of_v_0,
+        weight_index=weight_index,
+        weyl_orbit=weyl_orbit,
+        W_c_str=W_c_str,
+        roots=roots,
+        solitons_str=solitons_str,
+        v_c_str=v_c_str,
+        len_orbit=len_orbit,
     )
 
 
 @app.route('/plot')
 def coxeter_projection_plot():
-    cp = CoxeterProjection(
-        root_system=flask.session['root_system'], 
-        n_of_v_0=flask.session['n_of_v_0'],
-        weight_index=flask.session['weight_index'],                   
+    root_system = flask.request.args.get('root_system')
+    n_of_v_0_str = flask.request.args.get('n_of_v_0')
+    n_of_v_0 = int(n_of_v_0_str)
+    weight_index_str = flask.request.args.get('weight_index')
+    try:
+        weight_index = int(weight_index_str)
+    except:
+        weight_index = None
+
+    img = cproj.plot_coxeter_projection(
+        cproj.get_sage_data_str(root_system, n_of_v_0_str),
+        root_system=root_system, 
+        n_of_v_0=n_of_v_0,
+        weight_index=weight_index,                   
         is_interactive=False,
     )
-    cp.get_sage_data()
-    img = cp.plot()
     rv = flask.send_file(img, mimetype='image/png', cache_timeout=0)
     rv.set_etag(str(time.time()))
     return rv
@@ -55,13 +136,10 @@ def coxeter_projection_plot():
 
 def set_config_items(config_items, request_dict):
     for key, value in request_dict.items():
-        if key == 'weight_index' or key == 'n_of_v_0':
-            try:
-                config_items[key] = int(value)
-            except ValueError:
-                config_items[key] = None
-        else:
-            config_items[key] = value
+        config_items[key] = value
+    config_items['root_system'] = (
+        request_dict['type'] + request_dict['rank']
+    )
 
 if __name__ == '__main__':
     app.run()
